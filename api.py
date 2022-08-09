8d67fc2943444e48ec363d5e7e223d4968c6a399
brivazz <yateperna@gmail.com>
2022-08-09 08:06:06 +0300

    First commit

---
+++ api.py
@@ -0,0 +1,34 @@
+import shutil
+from typing import List
+from fastapi import APIRouter, UploadFile, File, Form
+
+from schemas import UploadVideo, GetVideo
+
+
+video_router = APIRouter()
+
+
+@video_router.post('/')
+async def root(title: str = Form(...), description: str = Form(...), file: UploadFile = File(...)):
+    info = UploadVideo(title=title, description=description)
+    with open(f"{file.filename}", 'wb') as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return {'file_name': file.filename, 'info': info}
+
+
+
+@video_router.post('/img')
+async def upload_image(files: List[UploadFile] = File(...)):
+    for img in files:
+        with open(f"{img.filename}", 'wb') as buffer:
+            shutil.copyfileobj(img.file, buffer)
+
+    return {'file_name': 'Good'}
+
+
+@video_router.get('/video')
+async def get_video():
+    user = {'id': 7, 'name': 'Pipec'}
+    video = {'title': 'Title', 'description': 'Description'}
+    return GetVideo(user=user, video=video)
---
+++ schemas.py
@@ -0,0 +1,18 @@
+from typing import List
+
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    id: int
+    name: str
+
+
+class UploadVideo(BaseModel):
+    title: str
+    description: str
+
+
+class GetVideo(BaseModel):
+    user: User
+    video: UploadVideo
